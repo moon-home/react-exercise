5a31bb997b35db50c2fd81b06781ea27a2a65c9c
moonmoonhome <moon30biology@gmail.com>
2020-05-16 23:36:36 -0400

    Updates

---
+++ precache-manifest.dbf124b622223eb2ba1099b48ca1a089.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6959d0168f5075eef74a7136ef9c5a56",
+    "url": "/React-basics/index.html"
+  },
+  {
+    "revision": "51d0fdfa55ee8583035a",
+    "url": "/React-basics/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/React-basics/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-basics/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51d0fdfa55ee8583035a",
+    "url": "/React-basics/static/js/main.7d879de6.chunk.js"
+  },
+  {
+    "revision": "3db46d76901f04de35fa",
+    "url": "/React-basics/static/js/runtime-main.297cddce.js"
+  }
+]);
